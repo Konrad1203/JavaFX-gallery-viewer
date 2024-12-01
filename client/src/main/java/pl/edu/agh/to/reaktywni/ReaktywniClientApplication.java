fbcc08f054d59c47de2244e1bc08abc8eab6fa47
Mateusz Bobula <matbobula@student.agh.edu.pl>
2024-12-01 18:51:06 +0100

    Added client and moved files

---
+++ client/src/main/java/pl/edu/agh/to/reaktywni/ReaktywniClientApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.to.reaktywni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReaktywniClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReaktywniClientApplication.class, args);
+	}
+
+}
