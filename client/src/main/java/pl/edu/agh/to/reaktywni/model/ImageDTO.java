008d774bd76de2d5e17929dabe3e2341187a65d2
Mateusz Bobula <matbobula@student.agh.edu.pl>
2024-12-07 18:52:11 +0100

    Efficiency improvment with loading big images
    improved for 2MB file to load from 3.5s to no time

--- client/src/main/java/pl/edu/agh/to/reaktywni/model/ImageDTO.java
+++ client/src/main/java/pl/edu/agh/to/reaktywni/model/ImageDTO.java
@@ -1,10 +1,13 @@
 package pl.edu.agh.to.reaktywni.model;
 
 import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.FileImageInputStream;
+import javax.imageio.stream.ImageInputStream;
 import java.io.File;
 import java.io.IOException;
-import java.io.ByteArrayOutputStream;
+import java.nio.file.Files;
+import java.util.Iterator;
 
 public class ImageDTO {
 
@@ -19,58 +22,57 @@ public class ImageDTO {
 
     public ImageDTO(int idOfPutting, File file) throws IOException {
         this.idOfPutting = idOfPutting;
-        readImageDetails(file);
+        readFileDetails(file);
+        setImageDimension(file);
     }
 
-    private void readImageDetails(File file) throws IOException {
-        BufferedImage bufferedImage = ImageIO.read(file);
-
-        if (bufferedImage == null) {
-            throw new IOException("Nie udało się wczytać obrazu z pliku: " + file.getPath());
-        }
-
+    private void readFileDetails(File file) throws IOException {
         this.name = file.getName();
         this.extensionType = getFileExtension(file.getName());
-        this.width = bufferedImage.getWidth();
-        this.height = bufferedImage.getHeight();
-
-        try (ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
-            ImageIO.write(bufferedImage, extensionType, baos);
-            this.data = baos.toByteArray();
-        }
+        this.data = Files.readAllBytes(file.toPath());
     }
 
     private String getFileExtension(String fileName) {
-        String extension = "";
         int dotIndex = fileName.lastIndexOf(".");
-        if (dotIndex > 0) {
-            extension = fileName.substring(dotIndex + 1);
+        return (dotIndex > 0) ? fileName.substring(dotIndex + 1) : "";
+    }
+
+    private void setImageDimension(File imgFile) throws IOException {
+        int pos = imgFile.getName().lastIndexOf(".");
+        if (pos == -1) {
+            throw new IOException("No extension for file: " + imgFile.getAbsolutePath());
         }
-        return extension;
+
+        String suffix = imgFile.getName().substring(pos + 1);
+        Iterator<ImageReader> iter = ImageIO.getImageReadersBySuffix(suffix);
+
+        if (!iter.hasNext()) {
+            throw new IOException("No suitable ImageReader for file: " + imgFile.getAbsolutePath());
+        }
+
+        while (iter.hasNext()) {
+            ImageReader reader = iter.next();
+            try (ImageInputStream stream = new FileImageInputStream(imgFile)) {
+                reader.setInput(stream);
+                this.width = reader.getWidth(reader.getMinIndex());
+                this.height = reader.getHeight(reader.getMinIndex());
+                return;
+            } catch (IOException e) {
+                throw new IOException("Error reading image dimensions for file: " + imgFile.getAbsolutePath(), e);
+            } finally {
+                reader.dispose();
+            }
+        }
+
+        throw new IOException("Not a valid image file: " + imgFile.getAbsolutePath());
     }
 
 
-    public int getIdOfPutting() {
-        return idOfPutting;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getExtensionType() {
-        return extensionType;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public byte[] getImageData() {
-        return data;
-    }
+    public int getIdOfPutting() { return idOfPutting; }
+    public String getName() { return name; }
+    public String getExtensionType() { return extensionType; }
+    public int getWidth(){ return width; }
+    public int getHeight(){ return height; }
+    public byte[] getImageData() { return data; }
 }
+
