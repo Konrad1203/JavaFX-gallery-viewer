29e26ca1ee21ad9790f9e6a0a7e1fd73916b9be4
Mateusz Bobula <matbobula@student.agh.edu.pl>
2024-12-07 17:43:11 +0100

    Increased limits for sending images (1000MB))
    took me 3h to find 8 lines of code to do it 💀

---
+++ server/src/main/java/pl/edu/agh/to/reaktywni/WebFluxConfig.java
@@ -0,0 +1,16 @@
+package pl.edu.agh.to.reaktywni;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.codec.ServerCodecConfigurer;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+
+
+@Configuration
+public class WebFluxConfig implements WebFluxConfigurer {
+    private final int howManyMB = 1000;
+    @Override
+    public void configureHttpMessageCodecs(ServerCodecConfigurer configurer) {
+        configurer.defaultCodecs().maxInMemorySize(howManyMB * 1024 * 1024);
+    }
+}
