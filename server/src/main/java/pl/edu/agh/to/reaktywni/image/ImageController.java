624b997c0bec267274c2996eff5c9a16c8797bd8
Mateusz Bobula <matbobula@student.agh.edu.pl>
2024-12-08 14:19:04 +0100

    Moved business logic from controller to service (Image))

--- server/src/main/java/pl/edu/agh/to/reaktywni/image/ImageController.java
+++ server/src/main/java/pl/edu/agh/to/reaktywni/image/ImageController.java
@@ -1,33 +1,18 @@
 package pl.edu.agh.to.reaktywni.image;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import pl.edu.agh.to.reaktywni.util.ImageResizer;
-import pl.edu.agh.to.reaktywni.util.Resizable;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-import reactor.core.scheduler.Scheduler;
-import reactor.core.scheduler.Schedulers;
-
-import java.io.IOException;
-import java.util.Arrays;
-
 
 @RestController
 @RequestMapping("/images")
 public class ImageController {
-    private final Resizable imageResizer;
     private final ImageService imageService;
 
-    @Autowired
-    public ImageController(ImageService imageService, Resizable imageResizer) {
+    public ImageController(ImageService imageService) {
         this.imageService = imageService;
-        this.imageResizer = imageResizer;
     }
 
     @GetMapping
@@ -37,17 +22,7 @@ public class ImageController {
 
     @PostMapping(consumes = MediaType.APPLICATION_NDJSON_VALUE, produces = MediaType.APPLICATION_NDJSON_VALUE)
     public Flux<ImageDTO> processImages(@RequestBody Flux<ImageDTO> images) {
-        return images
-                .doOnNext(imageDTO -> System.out.println("Received image: " + imageDTO.getName() + "\tWidth: " + imageDTO.getWidth() + "\tHeight: " + imageDTO.getHeight()))
-                .flatMap(imageDTO -> Mono.fromCallable(() -> imageResizer.resize(imageDTO, 100, 100))
-                        .subscribeOn(Schedulers.boundedElastic())
-                        .flatMap(optionalImageDTO -> Mono.justOrEmpty(optionalImageDTO)
-                                .switchIfEmpty(Mono.error(new IllegalArgumentException("Failed to resize image: " + imageDTO.getName())))
-                        )
-                ).doOnNext(imageDTO -> {
-                    System.out.println("Processed image: " + imageDTO.getName());
-                    System.out.println("Width: " + imageDTO.getWidth() + "\tHeight: " + imageDTO.getHeight());
-                });
+        return imageService.processImages(images);
     }
 
     @GetMapping("/{id}")
--- server/src/main/java/pl/edu/agh/to/reaktywni/image/ImageService.java
+++ server/src/main/java/pl/edu/agh/to/reaktywni/image/ImageService.java
@@ -1,13 +1,14 @@
 package pl.edu.agh.to.reaktywni.image;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import pl.edu.agh.to.reaktywni.thumbnail.Thumbnail;
 import pl.edu.agh.to.reaktywni.thumbnail.ThumbnailRepository;
+import pl.edu.agh.to.reaktywni.util.Resizable;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 import java.io.File;
 import java.io.IOException;
@@ -17,13 +18,29 @@ import java.nio.file.Paths;
 
 @Service
 public class ImageService {
-
     private final ImageRepository imageRepository;
     private final ThumbnailRepository thumbnailRepository;
+    private final Resizable imageResizer;
 
-    public ImageService(ImageRepository imageRepository, ThumbnailRepository thumbnailRepository) {
+    public ImageService(ImageRepository imageRepository, ThumbnailRepository thumbnailRepository, Resizable imageResizer) {
         this.imageRepository = imageRepository;
         this.thumbnailRepository = thumbnailRepository;
+        this.imageResizer = imageResizer;
+
+    }
+
+    public Flux<ImageDTO> processImages(Flux<ImageDTO> images){
+        return images.doOnNext(imageDTO -> System.out.println("Received image: " + imageDTO.getName() +
+                               "\tWidth: " + imageDTO.getWidth() + "\tHeight: " + imageDTO.getHeight())
+                ).flatMap(imageDTO -> Mono.fromCallable(() -> imageResizer.resize(imageDTO, 100, 100))
+                        .subscribeOn(Schedulers.boundedElastic())
+                        .flatMap(optionalImageDTO -> Mono.justOrEmpty(optionalImageDTO)
+                                .switchIfEmpty(Mono.error(new IllegalArgumentException("Failed to resize image: " + imageDTO.getName())))
+                        )
+                ).doOnNext(imageDTO -> {
+                    System.out.println("Processed image: " + imageDTO.getName());
+                    System.out.println("Width: " + imageDTO.getWidth() + "\tHeight: " + imageDTO.getHeight());
+                });
     }
 
     public int saveImage(Image image) {
