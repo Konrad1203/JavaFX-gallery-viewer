817feaa83df63d3b1b5be643da628c52963f475b
Bartosz <knapikbartek@gmail.com>
2025-01-08 16:45:27 +0100

    ImageController decomposition into ImageController and ThumbnailController

--- server/src/main/java/pl/edu/agh/to/reaktywni/image/ImageController.java
+++ server/src/main/java/pl/edu/agh/to/reaktywni/image/ImageController.java
@@ -8,11 +8,9 @@ import pl.edu.agh.to.reaktywni.thumbnail.ThumbnailSize;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import java.util.Set;
-import java.util.stream.Stream;
-
 
 @RestController
+@RequestMapping("/images")
 public class ImageController {
 
     private final ImageService imageService;
@@ -21,43 +19,18 @@ public class ImageController {
         this.imageService = imageService;
     }
 
-    @GetMapping("/images/{id}")
+    @GetMapping("/{id}")
     public Mono<Image> getImage(@PathVariable int id) {
         return imageService.getImage(id)
                 .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "Image not found")));
     }
 
-    @PostMapping(path="/images", consumes = MediaType.APPLICATION_NDJSON_VALUE, produces = MediaType.APPLICATION_NDJSON_VALUE)
+    @PostMapping(consumes = MediaType.APPLICATION_NDJSON_VALUE, produces = MediaType.APPLICATION_NDJSON_VALUE)
     public Flux<Image> postImages(@RequestBody Flux<Image> images, @RequestParam String size) {
         if (!ThumbnailSize.isValidSize(size)) {
             return Flux.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid thumbnail size"));
         }
         return imageService.processImages(images, size);
-    }
-
-    @GetMapping("/thumbnails")
-    public Flux<Image> getThumbnails(@RequestParam String size) {
-        if (!ThumbnailSize.isValidSize(size)) {
-            return Flux.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid thumbnail size"));
-        }
-        return imageService.getThumbnails(size);
-    }
-
-    @GetMapping("/thumbnails/excluding")
-    public Flux<Image> getThumbnailsExcludingSet(@RequestParam String size, @RequestParam Set<Integer> ids) {
-        if (!ThumbnailSize.isValidSize(size)) {
-            return Flux.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid thumbnail size"));
-        }
-        return imageService.getThumbnailsExcludingSet(size, ids);
-    }
-
-    @GetMapping("/thumbnails/count")
-    public Mono<Long> getThumbnailsCount(@RequestParam String size) {
-        if (!ThumbnailSize.isValidSize(size)) {
-            return Mono.error(new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid thumbnail size"));
-        }
-        return imageService.getThumbnailsCount(size)
-                .switchIfEmpty(Mono.error(new IllegalStateException("Cannot get thumbnails count")));
     }
 }
 
